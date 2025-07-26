f934a98f3faf87cee48b6a7facba683bb9d233eb
Kofidhope <forsonbentum55@gmail.com>
2025-07-26 14:23:50 +0000

    cart services updated

--- src/main/java/com/dhopecode/shoppingCart/service/cart/CartService.java
+++ src/main/java/com/dhopecode/shoppingCart/service/cart/CartService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +17,7 @@ public class CartService implements iCartService{
 
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final AtomicLong cartIdGenerator= new AtomicLong(0);
 
     @Override
     public Cart getCart(Long id) {
@@ -38,5 +40,12 @@ public class CartService implements iCartService{
     public BigDecimal getTotalPrice(Long id) {
         Cart cart = getCart(id);
         return cart.getTotalAmount();   
+    }
+
+    public long initialiseNewCart(){
+        Cart newCart = new Cart();
+        Long newCartId = cartIdGenerator.incrementAndGet();
+        newCart.setId(newCartId);
+        return cartRepository.save(newCart).getId();
     }
 }
