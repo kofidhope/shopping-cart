2bbe652f1da670348985e937dda4dc2c6f2891ab
Kofidhope <forsonbentum55@gmail.com>
2025-07-27 12:39:00 +0000

    user entity and relationships implemented

---
+++ src/main/java/com/dhopecode/shoppingCart/model/User.java
@@ -0,0 +1,28 @@
+package com.dhopecode.shoppingCart.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @OneToOne(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    private Cart cart;
+
+    @OneToMany(mappedBy = "User",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Order> orders;
+}
