56a3c0d63c9f0f42943f272ee806f46a8bf8de8b
Kofidhope <forsonbentum55@gmail.com>
2025-07-29 23:18:51 +0000

    jwt  added

--- src/main/java/com/dhopecode/shoppingCart/security/config/ShopConfig.java
+++ src/main/java/com/dhopecode/shoppingCart/security/config/ShopConfig.java
@@ -1,4 +1,4 @@
-package com.dhopecode.shoppingCart.config;
+package com.dhopecode.shoppingCart.security.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
